96777bb545c85f2b8dcb33103f0e732f2512e2c6
Mikayel Galyan <mikayel.galyan@gmail.com>
2022-05-29 12:11:35 +0400

    update return to semver

--- entrypoint.sh
+++ entrypoint.sh
@@ -64,7 +64,6 @@ case "$tag_context" in
     *repo*)
         lasttag="$(git describe --abbrev=0 --tags $(git rev-list --tags --max-count=1))"
         [ -z "$lasttag" ] || tag="$(semver -c $lasttag | tail -n 1)"
-        $pre_release && lastN="${lasttag##*.}"
         ;;
     *branch*)
         if [ -z "$prefix" ]
@@ -74,7 +73,6 @@ case "$tag_context" in
            lasttag="$(git tag --list --sort=-v:refname | grep -E "$fmt" | grep $prefix- | head -n 1)"
         fi
         [ -z "$lasttag" ] || tag="$(semver -c $lasttag | tail -n 1)"
-        $pre_release && lastN="${lasttag##*.}"
         ;;
     * ) echo "Unrecognised context"; exit 1;;
 esac
@@ -82,7 +80,6 @@ esac
 echo "Last tag info ..."
 echo -e "Found tag numeric... \t$tag"
 echo -e "Found full tag ... \t$lasttag"
-echo -e "If prerelease true last number \t$lastN"
 
 # save previous tag
 old=$tag
@@ -175,8 +172,7 @@ if $pre_release
 then
     # Already a prerelease available, bump it
     if [[ "$old" == *"$new"* ]]; then
-        lastN=$((lastN+1))
-		new="$new-$suffix.$lastN"
+        new=$(semver -i prerelease $new --preid $suffix); part="$part"
     else
         new="$new-$suffix.0"
     fi
